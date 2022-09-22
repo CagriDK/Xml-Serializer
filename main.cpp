630e29265392e328155ec93cc5e2b9debaec23fc
Çağrı Dükünlü <cagridknl@gmail.com>
2022-09-22 16:24:50 +0300

    Nested Class variable support added

--- main.cpp
+++ main.cpp
@@ -12,14 +12,16 @@ class DataStorage{
 public:
 
     struct classMembers{
-        classMembers():variableName(),variableType(memberType::Variable),nestedClass(false),levelOfNest(0){}
+        classMembers():variableName(),variableType(memberType::Variable){}
         enum class memberType{Variable=0,Function=1,Array=2,Struct=3,Class=4};
 
         std::string variableName;
         memberType variableType;
+    };
 
-        bool nestedClass;
-        int levelOfNest;
+    struct classListStructure{
+        std::string className;
+        std::vector<classMembers> classVariables;
     };
 
 public:
@@ -29,18 +31,33 @@ public:
         if(!xmlFile.is_open())
         {qWarning()<<"File couldn't opened";}
         std::string line;
-        std::vector<QString> QStringLineList; //Holds QStringLines for one class
-        std::vector<std::vector<QString>> classList; //Holds All the classes for QStringLinesList
         bool isNewClass=false;
         classCounter=0;
-
+        std::vector<QString> QStringLineList; //Holds QStringLines for one class
+        std::vector<std::vector<QString>> classQStringList; //Holds All the classes for QStringLinesList
+        std::vector<std::string> variableTypes={
+            "void","char","bool","int","double","float",
+            "unsigned long","signed long","long","short",
+            "signed short","unsigned short"};
         while(std::getline(xmlFile,line)){
             QString QStringLine=QString::fromStdString(line);
 
             if(QStringLine.contains("class "))
             {
-                classCounter++;
+                QStringLine.remove("class ");
+                QStringLine.remove("");
+                QStringLine.remove(" ");
+                QStringLine.remove(";");
+                QStringLine.remove("{");
+                if(QStringLine.contains(":"))
+                {
+                    QStringLine= QStringLine.mid(0, QStringLine.indexOf(":"));
+                }
+
+                tempClassNames.push_back(QStringLine.toStdString());
+                variableTypes.push_back(QStringLine.toStdString()); //Add classname to variable types, it could be a class data inside a data (Nested Classes);
                 isNewClass=true;
+                classCounter++;
             }
 
             if(isNewClass)
@@ -51,27 +68,62 @@ public:
             if(QStringLine.contains("};"))
             {
                 if(classCounter!=0){
-                    classList.push_back(QStringLineList);
+                    classQStringList.push_back(QStringLineList);
                 }
                 QStringLineList.clear();
                 isNewClass=false;
             }
-
-//            for(short i=0;i<static_cast<short>(variableTypes.size());i++)
-//            {
-//                getVariables(variableTypes.at(i),line);
-//            };
         }
 
-
+        for(unsigned long long i=0; i<classQStringList.size();i++)
+        {
+            for(unsigned long long j=0; j<classQStringList[i].size();j++)
+            {
+                for(short k=0;k<static_cast<short>(variableTypes.size());k++)
+                {
+                    qDebug()<<variableTypes.size();
+                    int variableTypeSize=variableTypes.size();
+                    getVariables(variableTypes.at(k),k,variableTypeSize,i,classQStringList[i][j].toStdString());
+                };
+            }
+        m_temp.className=tempClassNames.at(i);
+        classList.push_back(m_temp);
+        m_temp.classVariables.clear();
+        m_temp.className="";
+        }
     };
-    void getVariables(const std::string& n ,const std::string& m){
+
+    void getVariables(const std::string& n,int k,int variableTypeSize,int whichClass ,const std::string& m)
+    {
         QString QStringLine=QString::fromStdString(m);
-        classMembers temp;
 
         if(QStringLine.contains(QString::fromStdString(n)))
         {
-            if(QStringLine.contains("("))
+
+            classMembers temp;
+
+            if(k>=12){
+                int classCount=variableTypeSize-12; // 3 gelecek
+
+                if(!(k>=(variableTypeSize-classCount+whichClass))){
+                    //If different from predefined variable types check for class types. keyword:variableTypes {#predefinedtypes} extra means nested class present.
+                    QStringLine.remove(QString::fromStdString(n));
+                    QStringLine.remove("");
+                    QStringLine.remove(" ");
+                    QStringLine.remove(";");
+
+
+
+                    temp.variableName=QStringLine.toStdString();;
+                    temp.variableType=classMembers::memberType::Class;
+                }
+                else {
+                return;
+                }
+
+            }
+
+            else if(QStringLine.contains("("))
             {
                 QStringLine.remove("\t");
                 QStringLine.remove(QString::fromStdString(n));
@@ -125,29 +177,27 @@ public:
                 temp.variableName=QStringLine.toStdString();
                 temp.variableType=classMembers::memberType::Variable;
             }
-            classVariables.push_back(temp);
+            m_temp.classVariables.push_back(temp);
+            //classVariables.push_back(temp);
         }
     }
 private:
-    std::vector<std::string> variableTypes={
-        "void","char","bool","int","double","float",
-        "unsigned long","signed long","long","short",
-        "signed short","unsigned short"};
-    std::string temp_className;
     int classCounter; // Kaç adet Class olduğunu tutan değişken
-    std::vector<classMembers> classVariables; // Class Verileri
-    std::vector<std::vector<classMembers>> classList; //Class Verilerini Tutan Class
+     // Class Verileri
+    std::vector<classListStructure> classList;
+    classListStructure m_temp;
+    std::vector<std::string> tempClassNames;
 
+    //std::vector<std::vector<classMembers>> classList;
 };
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    std::string filePath="C:/Users/cagri/OneDrive/Documents/untitled7/mydocument.xml";
+    std::string filePath="C:/Projects/Xml-Serializer/mydocument.xml";
 //  std::replace(filePath.begin(), filePath.end(), '\\', '/');
     std::unique_ptr<DataStorage> t2=std::make_unique<DataStorage>();
     t2->readTextFile(filePath);
-    //classList=reaDFile(filePath);
 
     return a.exec();
 }
